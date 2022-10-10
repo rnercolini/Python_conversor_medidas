2b8ce31f47b60e82fe76418acc22ff9bf6a6b61b
rnercolini <106276254+rnercolini@users.noreply.github.com>
2022-10-10 14:08:28 -0300

    Add files via upload

---
+++ Desafio8.py
@@ -0,0 +1,5 @@
+# Conversor de medidas
+n = float(input(' Digite a distância em metros: '))
+c = n * 100
+mi = n * 1000
+print('{0} metros é o mesmo que {1:.0f} centímetros e {2:.0f} milímetros.'.format(n, c, mi))
